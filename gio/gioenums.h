d905f1f70ba2365760934a89f612bc2172054627
Guido Günther <agx@sigxcpu.org>
2026-05-01 13:10:49 +0200

    gioenums: G_APPLICATION_HANDLES_OPEN can be useful with G_APPLICATION_HANDLES_COMMAND_LINE
    Update the documentation accordingly.
    
    Closes: #1853
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- gio/gioenums.h
+++ gio/gioenums.h
@@ -1510,8 +1510,9 @@ typedef enum
  * @G_APPLICATION_IS_LAUNCHER: Don't try to become the primary instance.
  * @G_APPLICATION_HANDLES_OPEN: This application handles opening files (in
  *     the primary instance). Note that this flag only affects the default
- *     implementation of local_command_line(), and has no effect if
- *     %G_APPLICATION_HANDLES_COMMAND_LINE is given.
+ *     implementation of local_command_line(). It can be useful even when
+ *     using `G_APPLICATION_HANDLES_COMMAND_LINE` to handle
+ *     `org.freedesktop.Application.open`.
  *     See g_application_run() for details.
  * @G_APPLICATION_HANDLES_COMMAND_LINE: This application handles command line
  *     arguments (in the primary instance). Note that this flag only affect
